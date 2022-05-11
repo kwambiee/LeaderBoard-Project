aee24971baf1b233b8a8697dfda94c11ea4d58a6
Kwamboka <kwambokaj2.jk@gmail.com>
2022-05-11 22:07:52 +0300

    Fix errors

--- src/index.js
+++ src/index.js
@@ -1,42 +1,17 @@
 // css files here
 import './style.css';
-import Leaderboard from './modules/addscore.js';
-import postData from './modules/game.js';
+import { addScore, getScore } from './modules/leaderBoard.js';
 import * as Element from './modules/elements.js';
 
-const newScore = new Leaderboard();
-
-const getData = async () => {
-  try {
-    let result = await postData();
-    console.log(result);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-getData();
-
-const displayScore = (scores) => {
-  let content = '';
-  scores.scoreArray.forEach((element) => {
-    content += `
-    <li>${element.nameInput}:${element.marksInput}</li>`;
-  });
-  Element.scoreTable.innerHTML = content;
-};
-displayScore(newScore);
+Element.refresh.addEventListener('click', getScore);
 
 Element.form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const nameInput = Element.form.elements.names.value;
-  const marksInput = Element.form.elements.marks.value;
-  if (nameInput && marksInput) {
-    newScore.addScore(nameInput, marksInput);
-    newScore.updateScore();
-    displayScore(newScore);
-    Element.form.elements.marks.value = '';
-    Element.form.elements.names.value = '';
-  }
+  const nameInput = Element.form.names.value;
+  const scoreInput = Element.form.marks.value;
+  const userscore = { user: `${nameInput}`, score: `${scoreInput}` };
+
+  addScore(userscore).then(() => {
+    console.log(getScore());
+  });
 });
-displayScore(newScore);
